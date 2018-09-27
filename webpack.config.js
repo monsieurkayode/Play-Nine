7edac3c98734b1f95d274b8155649988d0936cb6
Kayode Adeola <monsieurkayode@gmail.com>
2018-09-27 19:39:28 +0100

    chore(setup): install and setup development tools (#1)
    - initilaize project package using yarn
    - install application dependencies
    - setup eslint
    - add babel configuration
    - setup webpack configuration for bundling
    - add editor configuration
    
    [Finishes #60827919]

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const { resolve, join } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body',
+});
+
+module.exports = {
+  context: __dirname,
+  mode: 'development',
+  devtool: 'source-map',
+  entry: [
+    resolve(__dirname, './src/index'),
+  ],
+  output: {
+    path: join(__dirname, 'dist'),
+    filename: 'js/bundle.js',
+  },
+  resolve: {
+    modules: ['node_modules', './src'],
+    extensions: ['.js', '.jsx'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader'],
+        exclude: [/node_modules/],
+      },
+    ],
+  },
+  plugins: [
+    HtmlWebpackPluginConfig,
+  ],
+  devServer: {
+    publicPath: '/',
+    historyApiFallback: true,
+  },
+};
