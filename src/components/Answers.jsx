0a4174ad57a86de4ca88bf3a1ef979d908ee273c
Kayode Adeola <monsieurkayode@gmail.com>
2018-09-28 00:34:14 +0100

    feat(selectNumber): implement user select number (#3)
    - make the Game component stateful
    - create an empty array state for selected numbers
    - create selectNumber action on the game class
    - pass down selected numbers and selectNumber action to
    child Numbers
    - create an onclick event to be invoked from Numbers component
    - display selected numbers and make them unselectable
    
    [Delivers #160837903]

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Number } from './Numbers';
 
-const Answers = () => (
+const Answers = ({ selectedNumbers }) => (
   <div className="col-5 text-center">
-    <Number>2</Number>
-    <Number>5</Number>
+    { selectedNumbers.map(number => <Number key={number}>{number}</Number>)}
   </div>
 );
+
+Answers.propTypes = {
+  selectedNumbers: PropTypes.arrayOf(PropTypes.number).isRequired,
+};
 
 export default Answers;
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import Stars from './Stars';
 import Button from './Button';
@@ -38,25 +38,48 @@ const Header = styled.header`
   }
 `;
 
-const Game = () => (
-  <Wrapper>
-    <Header>
-      <h3>
-        <span>Play</span>
-        Nine
-        <sup>9</sup>
-      </h3>
-    </Header>
-    <Container className="container">
-      <div className="row">
-        <Stars />
-        <Button />
-        <Answers />
-      </div>
-      <br />
-      <Numbers />
-    </Container>
-  </Wrapper>
-);
+class Game extends Component {
+  state = {
+    selectedNumbers: [],
+  }
+
+  selectNumber = (clickedNumber) => {
+    const { selectedNumbers } = this.state;
+    if (selectedNumbers.includes(clickedNumber)) { return; }
+
+    this.setState(prevState => ({
+      selectedNumbers: prevState.selectedNumbers.concat(clickedNumber),
+    }));
+  }
+
+  render() {
+    const { selectedNumbers } = this.state;
+    return (
+      <Wrapper>
+        <Header>
+          <h3>
+            <span>Play</span>
+            Nine
+            <sup>9</sup>
+          </h3>
+        </Header>
+        <Container className="container">
+          <div className="row">
+            <Stars />
+            <Button />
+            <Answers
+              selectedNumbers={selectedNumbers}
+            />
+          </div>
+          <br />
+          <Numbers
+            selectNumber={this.selectNumber}
+            selectedNumbers={selectedNumbers}
+          />
+        </Container>
+      </Wrapper>
+    );
+  }
+}
 
 export default Game;
