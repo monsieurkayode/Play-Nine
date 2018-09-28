a6e77ba4b855866e30c5fd62013b1c519aa98bd0
Kayode Adeola <monsieurkayode@gmail.com>
2018-09-28 01:16:36 +0100

    feat(removeNumber): implement unselecting selected number(s) (#4)
    - add a removeNumber action in the game component
    - pass action down as props to answers component
    - invoke action using an onclick event to remove selected number
    - ensure removed numbers are selectable from numbers
    
    [Delivers #160839527]

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Number } from './Numbers';
 
-const Answers = ({ selectedNumbers }) => (
+const Answers = ({ selectedNumbers, removeNumber }) => (
   <div className="col-5 text-center">
-    { selectedNumbers.map(number => <Number key={number}>{number}</Number>)}
+    { selectedNumbers.map(number => (
+      <Number
+        onClick={() => removeNumber(number)}
+        key={number}
+      >
+        {number}
+      </Number>))}
   </div>
 );
 
 Answers.propTypes = {
   selectedNumbers: PropTypes.arrayOf(PropTypes.number).isRequired,
+  removeNumber: PropTypes.func.isRequired,
 };
 
 export default Answers;
